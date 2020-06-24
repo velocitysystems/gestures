1e203d9a6285c5585149ee096be0b70e21aa1815
Matthew Richardson <matthew@velocitysystems.co.nz>
2020-06-24 09:00:11 +0100

    Scaffold macOS projects.

---
+++ samples/Gestures.Forms.Sample.MacOS/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Gestures.Forms.Sample.MacOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
---
+++ samples/Gestures.Sample.MacOS/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Gestures.Sample.MacOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
